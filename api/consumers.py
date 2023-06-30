ccf2864edb65c7dea47888603a14e74b35f6a3f2
PradeeprajDhanasekaran <rjpradeep820@gmail.com>
2023-07-01 01:54:46 +0530

    test for 1 to 1 chat

--- api/consumers.py
+++ api/consumers.py
@@ -6,12 +6,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         my_id = self.scope['user'].id
-        other_user_id = self.scope['url_route']['kwargs']['id']
-        if int(my_id) > int(other_user_id):
-            self.room_name = f'{my_id}-{other_user_id}'
-        else:
-            self.room_name = f'{other_user_id}-{my_id}'
-
+        # other_user_id = self.scope['url_route']['kwargs']['id']
+        # if int(my_id) > int(other_user_id):
+        #     self.room_name = f'{my_id}-{other_user_id}'
+        # else:
+        #     self.room_name = f'{other_user_id}-{my_id}'
+        self.room_name = my_id
         self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
@@ -24,11 +24,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         message = data['message']
         username = data['username']
-        receiver = data['receiver']
+        # receiver = data['receiver']
+        other_user_id = self.scope['url_route']['kwargs']['id']
+        receiver =  'chat_%s' % other_user_id
 
         # await self.save_message(username, self.room_group_name, message, receiver)
         await self.channel_layer.group_send(
-            self.room_group_name,
+            receiver ,
             {
                 "type": "chat_message",
                 "message": message,
--- api/routing.py
+++ api/routing.py
@@ -5,6 +5,7 @@ from django.core.asgi import get_asgi_application
 
 websocket_urlpatterns = [
     path('api/chat/send/<int:id>', consumers.ChatConsumer.as_asgi()),
+    path('api/chat/send/', consumers.ChatConsumer.as_asgi()),
 ]
 
 
